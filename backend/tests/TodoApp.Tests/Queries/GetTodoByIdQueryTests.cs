3beac1d15155afb83be8e8856c6a701b471cb27e
hkaab <hkaabi@myyahoo.com>
2026-06-25 17:34:36 +1000

    wip

--- backend/tests/TodoApp.Tests/Queries/GetTodoByIdQueryTests.cs
+++ backend/tests/TodoApp.Tests/Queries/GetTodoByIdQueryTests.cs
@@ -8,7 +8,7 @@ using Xunit;
 
 namespace TodoApp.Application.Tests.Queries
 {
-   public class GetTodoByIdQueryTests
+    public class GetTodoByIdQueryTests
     {
         [Fact]
         public void GetTodoByIdQuery_Should_Have_Correct_Id()
@@ -20,14 +20,6 @@ namespace TodoApp.Application.Tests.Queries
             var actualId = query.Id;
             // Assert
             Assert.Equal(expectedId, actualId);
-        }
-        [Fact]
-        public void GetTodoByIdQuery_Should_Throw_Exception_For_Empty_Id()
-        {
-            // Arrange
-            var emptyId = Guid.Empty;
-            // Act & Assert
-            Assert.Throws<ArgumentException>(() => new GetTodoByIdQuery(emptyId));
         }
     }
 }
